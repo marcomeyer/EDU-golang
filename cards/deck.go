966fb41f9b7a3177869d317accac817c4ca54715
Marco Meyer <marco.meyer@finnova.com>
2021-02-13 22:01:05 +0100

    function toString

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type deck []string
 
@@ -31,4 +34,8 @@ func deal(d deck, handSize int) (deck, deck) {
 	rest := d[handSize:]
 
 	return hand, rest
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
 }
